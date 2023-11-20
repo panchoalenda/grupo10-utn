acd73df452c873a1eadb70a6b1c863f7a9e5b17c
Francisco Alenda <pancho_alenda@yahoo.com.ar>
2023-11-20 16:56:30 +0100

    Creando Sistema de Reporte de Incidentes v1.0.0

---
+++ src/main/java/com/grupo10/app/ReporteApplication.java
@@ -0,0 +1,13 @@
+package com.grupo10.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo10/app/ReporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo10.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
